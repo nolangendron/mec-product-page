8d2162b410d171da3b37334862205aed504260d8
nolangendron <nolanglengendron@gmail.com>
2020-07-11 12:02:48 -0700

    Adds prettierrc file

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
-import { SearchInput } from "../components/SearchInput";
-import { makeApiCall } from "../utils/apiCall";
+import React, { useState } from 'react'
+import { SearchInput } from '../components/SearchInput'
+import { ProductsList } from '../components/ProductsList'
+import { makeApiCall } from '../utils/apiCall'
 
 export const LandingPage = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([])
 
   const handleSearch = (e, searchQuery) => {
-    e.preventDefault();
+    e.preventDefault()
     makeApiCall(searchQuery).then((results) => {
-      const data = results;
-      console.log(data);
-      setProducts(data);
-    });
-  };
+      const data = results
+      console.log(data)
+      setProducts(data)
+    })
+  }
 
-  return <SearchInput handleSearch={handleSearch} />;
-};
+  return <SearchInput handleSearch={handleSearch} />
+}
